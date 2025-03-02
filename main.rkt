9a08a2e50ec087454665e20cd9eab626599fb735
JJ <git@toki.la>
2025-03-02 14:43:31 -0800

    move resume macros + docs lower down as they are now more complicated

--- main.rkt
+++ main.rkt
@@ -20,6 +20,58 @@
   ;; to the nearest effect handler in the call stack with abort/cc
   (call/comp (λ (k) (abort/cc cio effect k)) cio))
 
+;; The with-effect-handler procedure is defined on analogy to r7rs's with-exception-handler.
+;; It takes in a handler procedure expecting a value and a continuation,
+;; and a zero-argument thunk being the computation to be handled.
+;; This is no more than a layer over call/prompt.
+;; As seen in the try macro definition, it is *very* close to shallow handlers.
+(define (with-effect-handler handler action)
+  (call/prompt (λ (_) (action)) cio handler (void)))
+
+;; The try macro is another useful primitive.
+;; It takes a *computation*, which may raise an *effect*, and
+;; a series of effect *patterns* and associated *handlers*.
+(define-syntax try
+  (syntax-rules ()
+    [(try #:shallow computation [pattern handler ...] ...)
+      (with-effect-handler
+        ;; If the computation jumps to the prompt tag: we have a suspended effect!
+        (λ (effect k)
+          ;; Match on the suspended effect, with the provided patterns/handlers.
+          (match effect
+            ;; If the effect matches a pattern, run the handler.
+            ;; This may use the suspended continuation (if resume is called).
+            ;; We must pass the necessary implicit parameters to the handler.
+            [pattern (syntax-parameterize ([try-k #'k] [try-run #f])
+              handler ...)] ...
+            ;; If the effect matches no patterns, re-suspend up the call stack.
+            ;; We must invoke k so as to not lose the initial site of suspension.
+            ;; Alternatively, we could write (abort/cc cio effect k) directly.
+            [_ (k (suspend effect))]))
+        ;; Wrap the computation in call/prompt, and evaluate it.
+        ;; If the computation completes without jumping to the prompt: return the value
+        (thunk computation))]
+
+    ;; Our implementation of deep handlers is much the same as the above,
+    ;; but we wrap the handlers themselves in a new layer of call/prompt.
+    [(try #:deep computation [pattern handler ...] ...)
+      (let ()
+        (define (run action)
+          (with-effect-handler
+            (λ (effect k)
+              (match effect
+                ;; Here, we wrap the handler body in our call/prompt again.
+                [pattern (syntax-parameterize ([try-k #'k] [try-run #'run])
+                  handler ...)] ...
+                ;; We do NOT wrap the unhandled case in call/prompt, so as to not loop.
+                [_ (k (suspend effect))]))
+            action))
+      (run (thunk computation)))]
+
+    ;; I like deep handlers a lot more than shallow handlers. So they're the default.
+    [(try computation [pattern handler ...] ...)
+      (try #:deep computation [pattern handler ...] ...)]))
+
 ;; A helpful parameter for ensuring resume is only used in appropriate contexts.
 ;; Does double duty in abstracting away the continuation invoked by resume.
 (define-syntax-parameter try-k #f)
@@ -75,58 +127,6 @@
         "must be used within the body of a try handler"
       #:attr k (syntax-parameter-value #'try-k)
       #'(call-in-continuation k (thunk (suspend effect)))]))
-
-;; The with-effect-handler procedure is defined on analogy to r7rs's with-exception-handler.
-;; It takes in a handler procedure expecting a value and a continuation,
-;; and a zero-argument thunk being the computation to be handled.
-;; This is no more than a layer over call/prompt.
-;; As seen in the try macro definition, it is *very* close to shallow handlers.
-(define (with-effect-handler handler action)
-  (call/prompt (λ (_) (action)) cio handler (void)))
-
-;; The try macro is another useful primitive.
-;; It takes a *computation*, which may raise an *effect*, and
-;; a series of effect *patterns* and associated *handlers*.
-(define-syntax try
-  (syntax-rules ()
-    [(try #:shallow computation [pattern handler ...] ...)
-      (with-effect-handler
-        ;; If the computation jumps to the prompt tag: we have a suspended effect!
-        (λ (effect k)
-          ;; Match on the suspended effect, with the provided patterns/handlers.
-          (match effect
-            ;; If the effect matches a pattern, run the handler.
-            ;; This may use the suspended continuation (if resume is called).
-            ;; We must pass the necessary implicit parameters to the handler.
-            [pattern (syntax-parameterize ([try-k #'k] [try-run #f])
-              handler ...)] ...
-            ;; If the effect matches no patterns, re-suspend up the call stack.
-            ;; We must invoke k so as to not lose the initial site of suspension.
-            ;; Alternatively, we could write (abort/cc cio effect k) directly.
-            [_ (k (suspend effect))]))
-        ;; Wrap the computation in call/prompt, and evaluate it.
-        ;; If the computation completes without jumping to the prompt: return the value
-        (thunk computation))]
-
-    ;; Our implementation of deep handlers is much the same as the above,
-    ;; but we wrap the handlers themselves in a new layer of call/prompt.
-    [(try #:deep computation [pattern handler ...] ...)
-      (let ()
-        (define (run action)
-          (with-effect-handler
-            (λ (effect k)
-              (match effect
-                ;; Here, we wrap the handler body in our call/prompt again.
-                [pattern (syntax-parameterize ([try-k #'k] [try-run #'run])
-                  handler ...)] ...
-                ;; We do NOT wrap the unhandled case in call/prompt, so as to not loop.
-                [_ (k (suspend effect))]))
-            action))
-      (run (thunk computation)))]
-
-    ;; I like deep handlers a lot more than shallow handlers. So they're the default.
-    [(try computation [pattern handler ...] ...)
-      (try #:deep computation [pattern handler ...] ...)]))
 
 ; References:
 ; https://github.com/tonyg/racket-effects/
